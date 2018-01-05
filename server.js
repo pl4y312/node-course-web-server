61f780ab5ad88a903d0eefe530b684f859046521
pl4y312 <pl4y312>
2018-01-05 19:22:02 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintenance')
+// })
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Homepage',
+    welcomeMessage: 'Welcome to my blog'
+  })
+})
+
+app.get('/about', (req, res) => {
+  const data = {
+    title: 'About Page'
+  }
+  res.render('about', data)
+})
+
+app.listen(3000, () => {
+  console.log('Server nodejs is up and running on port 3000');
+})
